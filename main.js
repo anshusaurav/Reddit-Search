3fe1b038cef5669a2cc44bfc87b0910723c9f28f
anshusaurav <anshu.saurav@gmail.com>
2020-04-04 19:19:32 +0530

    'Add-header-html'

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 async function showTopic(topicName) {
 
+    try{
     // read our JSON
-    let response = await fetch(`https://api.reddit.com/r/${topicName}`);
+    let response = await fetch(`https://api.reddit.com/r
+    /${topicName}`);
     let posts = await response.json();
-  
-    //console.log(posts)
     return posts;
+    }catch(error){
+        console.log(error.reason);
+    };
   }
   
