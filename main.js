10b674b9d6513496509ef3978a8d2dfe373b9c21
anshusaurav <anshu.saurav@gmail.com>
2020-04-04 22:06:36 +0530

    'Add-images'

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+
 async function showTopic(topicName) {
     let posts;
-    // read our JSON
     let response = await fetch(`https://api.reddit.com/r/${topicName}`);
     posts = await response.json();
     return posts;
-  }
+}
   
